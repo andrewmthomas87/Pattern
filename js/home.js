61171fc67561dbe5cc1877fa56bef1248aa4d363
Andrew Thomas <andrewmthomas87@gmail.com>
2015-01-31 18:29:18 -0700

    Made section square

--- js/home.js
+++ js/home.js
@@ -1,4 +1,10 @@
 
+$(document).ready(function() {
+	var width = min($(window).width(), $(window).height());
+	$('section').css('width', width);
+	$('section').css('height', width);
+});
+
 document.addEventListener('deviceready', function() {
 
 }, false);
