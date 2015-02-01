b886379986f5b987b179ab7f99c4310033f8e2a0
Andrew Thomas <andrewmthomas87@gmail.com>
2015-01-31 22:19:19 -0700

    Changed active colors and made section smaller

--- js/home.js
+++ js/home.js
@@ -4,16 +4,18 @@ function resize() {
 	var windowHeight = $(window).height();
 	if (windowWidth > windowHeight) {
 		$('section').css({
-			'width': windowHeight,
-			'height': windowHeight,
-			'padding': ''
+			'width': 'calc(' + windowHeight + 'px - 25%)',
+			'height': 'calc(' + windowHeight + 'px - 25%)',
+			'padding': '12.5% 0',
+			'margin-top': ''
 		});
 	}
 	else {
 		$('section').css({
-			'width': windowWidth,
-			'height': windowWidth,
-			'padding': ((windowHeight - windowWidth) / 2) + 'px 0'
+			'width': 'calc(' + windowWidth + 'px - 25%)',
+			'height': 'calc(' + windowWidth + 'px - 25%)',
+			'padding': '0 12.5%',
+			'margin-top': ((windowHeight - windowWidth) / 2) + 'px'
 		});
 	}
 	$('img#play').css('left', (($(window).width() - $('img#play').width()) / 2) + 'px');
