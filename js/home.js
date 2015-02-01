8b81bd1f4e53b663fed16fccd86ed551c3905f9e
Andrew Thomas <andrewmthomas87@gmail.com>
2015-01-31 18:41:12 -0700

    Fixed spelling error

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('deviceready', function() {
 	alert('Device ready');
-	var width = min($(window).width(), $(window).height());
+	var width = Math.min($(window).width(), $(window).height());
 	$('section').css('width', width);
 	$('section').css('height', width);
 	$('section').show('fast');
