943957bd805b7766f8e3caffe601b3c5e4265c4b
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-01 14:45:31 -0700

    Debugging

--- js/home.js
+++ js/home.js
@@ -7,8 +7,8 @@ function resize() {
 		'height': windowWidth + 'px',
 		'padding-top': (5 * (windowHeight - windowWidth) / 8) + 'px'
 	});
-	$('h3').css('top', (0.4 * windowHeight / 5) + 'px');
-	$('h4').css('top', 'calc(' + (0.4 * windowHeight / 5) + 'px + 1em)');
+	$('h3').css('top', 'calc(50% - ' + (0.2 * windowHeight) + 'px - 1.75em)');
+	$('h4').css('top', 'calc(50% - ' + (0.2 * windowHeight) + 'px - 0.875em)');
 	$('img#play').css('left', ((windowWidth - $('img#play').width()) / 2) + 'px');
 }
 
