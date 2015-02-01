2b66a154bc1da15bdcaf819367a7e8a242a4882e
Andrew Thomas <andrewmthomas87@gmail.com>
2015-01-31 18:34:53 -0700

    Debugging

--- js/home.js
+++ js/home.js
@@ -1,7 +1,9 @@
 
 document.addEventListener('deviceready', function() {
+	alert('Device ready');
 	var width = min($(window).width(), $(window).height());
 	$('section').css('width', width);
 	$('section').css('height', width);
 	$('section').show('fast');
+	alert('Debug');
 }, false);
